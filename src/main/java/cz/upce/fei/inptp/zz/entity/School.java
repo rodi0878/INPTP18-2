392e8166a4b9fdb93b87ac36133139424f466595
tomaskodym <st47115@student.upce.cz>
2018-11-22 17:28:55 +0100

    Update from original (#1)
    * Implementation of addCourse method considering the requirements
    
    * Adding a check that the course being entered is null

--- src/main/java/cz/upce/fei/inptp/zz/entity/School.java
+++ src/main/java/cz/upce/fei/inptp/zz/entity/School.java
@@ -8,40 +8,40 @@ import java.util.List;
  */
 public class School /*implements ISchool*/ {
 
-  private List<Course> courses;
-  private List<Teacher> teachers;
-  private List<Student> students;
+    private List<Course> courses;
+    private List<Teacher> teachers;
+    private List<Student> students;
 
-  public School() {
-    courses = new ArrayList<>();
-    teachers = new ArrayList<>();
-    students = new ArrayList<>();
-  }
-
-  // TODO: check if specific timeSlot action is available in course
-  // TODO: check if course action has a free capacity for new student
-  public boolean addStudentToCourseAction(Course course, Student student, TimeSlot timeSlot) {
-
-    //check if student is present at school
-    if (students.stream().noneMatch(studentAtSchool -> studentAtSchool.equals(student))) {
-      return false;
+    public School() {
+        courses = new ArrayList<>();
+        teachers = new ArrayList<>();
+        students = new ArrayList<>();
     }
 
-    //check if course is present at school
-    if (courses.stream().noneMatch(courseAtSchool -> courseAtSchool.equals(course))) {
-      return false;
-    }
+    // TODO: check if specific timeSlot action is available in course
+    // TODO: check if course action has a free capacity for new student
+    public boolean addStudentToCourseAction(Course course, Student student, TimeSlot timeSlot) {
 
-    for (CourseAction action : course.getActions()) {
-      if (action.getTimeSlot().equals(timeSlot)) {
-        action.getStudents().add(student);
-        student.getActions().add(action);
+        //check if student is present at school
+        if (students.stream().noneMatch(studentAtSchool -> studentAtSchool.equals(student))) {
+            return false;
+        }
 
-        return true;
-      }
+        //check if course is present at school
+        if (courses.stream().noneMatch(courseAtSchool -> courseAtSchool.equals(course))) {
+            return false;
+        }
+
+        for (CourseAction action : course.getActions()) {
+            if (action.getTimeSlot().equals(timeSlot)) {
+                action.getStudents().add(student);
+                student.getActions().add(action);
+
+                return true;
+            }
+        }
+        return false;
     }
-    return false;
-  }
 
     public List<Course> getCourses() {
         return courses;
@@ -65,6 +65,87 @@ public class School /*implements ISchool*/ {
 
     public void setStudents(List<Student> students) {
         this.students = students;
+    }
+
+    private boolean checkIsStudentAtSchool(Student student) {
+        for (Student studentOfSchool : students) {
+            if (studentOfSchool.equals(student)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkIsCourseAtSchool(Course course) {
+        for (Course courseOfSchool : courses) {
+            if (courseOfSchool.equals(course)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkAllStudentsOfCourseArePresentAtSchool(Course newCourse) {
+        for (CourseAction courseAction : newCourse.getActions()) {
+            for (Student student : courseAction.getStudents()) {
+                if (!checkIsStudentAtSchool(student)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean checkNoCollisionsWithTeachersAndTimeSlot(Course newCourse) {
+        for (Course course : courses) {
+            for (CourseAction courseAction : course.getActions()) {
+                for (CourseAction newCourseAction : newCourse.getActions()) {
+                    if (courseAction.getTeacher().equals(newCourseAction.getTeacher())
+                            && courseAction.getTimeSlot().equals(newCourseAction.getTimeSlot())) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean checkNoCollisionsWithStudentsAndTimeSlot(Course newCourse) {
+        for (Course course : courses) {
+            for (CourseAction courseAction : course.getActions()) {
+                for (CourseAction newCourseAction : newCourse.getActions()) {
+                    for (Student student : courseAction.getStudents()) {
+                        for (Student studentOfNewCourseAction : newCourseAction.getStudents()) {
+                            if (student.equals(studentOfNewCourseAction)
+                                    && courseAction.getTimeSlot().equals(newCourseAction.getTimeSlot())) {
+
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public void addCourse(Course newCourse) {
+        if (newCourse == null) {
+            throw new NullPointerException();
+        }
+        if (checkIsCourseAtSchool(newCourse)) {
+            throw new IllegalArgumentException("The course is already present at school");
+        }
+        if (!checkAllStudentsOfCourseArePresentAtSchool(newCourse)) {
+            throw new IllegalArgumentException("Student is not present at school");
+        }
+        if (!checkNoCollisionsWithTeachersAndTimeSlot(newCourse)) {
+            throw new IllegalArgumentException("Teacher has another action at this time");
+        }
+        if (!checkNoCollisionsWithStudentsAndTimeSlot(newCourse)) {
+            throw new IllegalArgumentException("Student has another action at this time");
+        }
+        courses.add(newCourse);
     }
 
 }
