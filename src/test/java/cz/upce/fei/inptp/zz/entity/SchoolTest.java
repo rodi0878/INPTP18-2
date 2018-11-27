4389170e099f5f6bb7b73addf97cfe94283ce6ff
nomad <st46619@student.upce.cz>
2018-11-27 21:00:20 +0100

    course action test moved to class CourseActionTest

--- src/test/java/cz/upce/fei/inptp/zz/entity/SchoolTest.java
+++ src/test/java/cz/upce/fei/inptp/zz/entity/SchoolTest.java
@@ -208,40 +208,6 @@ public class SchoolTest {
     }
     
     @Test
-    public void testHasCapacityOnEmptyCourseAction() {
-        CourseAction courseAction = new CourseAction();
-        courseAction.setCapacity(1);
-        
-        assertTrue(courseAction.hasCapacity());
-    }
-    
-    @Test
-    public void testHasCapacityOnZeroCapacityCourseAction() {
-        CourseAction courseAction = new CourseAction();
-        courseAction.setCapacity(0);
-        
-        assertFalse(courseAction.hasCapacity());
-    }
-    
-    @Test
-    public void testHasCapacityOnNotEmptyCourseAction() {
-        CourseAction courseAction = new CourseAction();
-        courseAction.setCapacity(2);
-        courseAction.getStudents().add(new Student());
-        
-        assertTrue(courseAction.hasCapacity());
-    }
-    
-    @Test
-    public void testHasCapacityOnFullCourseAction() {
-        CourseAction courseAction = new CourseAction();
-        courseAction.setCapacity(1);
-        courseAction.getStudents().add(new Student());
-        
-        assertFalse(courseAction.hasCapacity());
-    }
-    
-    @Test
     public void testAddStudentToEmptyCourseAction() {
         Student student = new Student();
         CourseAction courseAction = new CourseAction();
